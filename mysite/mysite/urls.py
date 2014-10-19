2714b328558ca4c4b977e22eeb78dd79232645a4
Rahul Bhandari <mr.bhandari@gmail.com>
2014-10-18 23:21:38 -0700

    got results page working with dummy data

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from mysite.views import hello, search, autosuggest
+from mysite.views import search, autosuggest
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -7,31 +7,6 @@ import MySQLdb
 from search.models import *
 import json
 
-
-
-
-def getResults(hcpcs, state):
-  db = MySQLdb.connect(host="localhost", # your host, usually localhost
-                     user="root", # your username
-                      passwd="thebakery", # your password
-                      db="doctors") # name of the data base
-
-  cur = db.cursor() 
-
-  # Use all the SQL you like
-  cur.execute("SELECT npi,first_name, last_name, num_services, hcpcs_description, city, state FROM medicare where hcpcs_code='" + hcpcs + "' and state = '" + state + "' ORDER BY num_services DESC LIMIT 100")
-
-  # print all the first cell of all the rows
-  
-  rows = cur.fetchall()
-  cur.close()
-  return rows
-
-def state_autosuggest(request):
-    if 'term' in request.GET:
-        search = request.GET['term']
-        autores = [AK, AL, AR, AZ, CA, CO, CT, DC, DE, FL, GA, HI, IA, ID, IL, IN, KS, KY, LA, MA, MD, ME, MI, MN, MO, MS, MT, NC, ND, NE, NH, NJ, NM, NV, NY, OH, OK, OR, PA, RI, SC, SD, TN, TX, UT, VA, VT, WA, WI, WV, WY]
-        return HttpResponse(json.dumps(autores), mimetype='application/json')
     
 def autosuggest(request):
     query = request.GET.get('q','')
@@ -49,58 +24,17 @@ def autosuggest(request):
 
 def search(request):
     if 'q' in request.GET:
-        #if 'hcpcs' in request.GET and request.GET['hcpcs']:
-        #    hcpcs = request.GET['hcpcs']
-        #    hcpcs = hcpcs.split(' -', 1)[0]
-        #    print "DONE!!"
-        #    print hcpcs
-        #    state = request.GET['state']
-        #    #providers = Medicare.objects.filter(hcpcs_code__icontains=hcpcs, state__icontains=state)
-        #    #print providers[1].npi
-        #    #print providers
-        #    
-	results = ['read', 'write']
-        print results
-            #products = Product.objects.filter(description__icontains=q)
-            #print products[1].brand
-            #print len(products)
-        #if 'brand' in request.GET and request.GET['brand']:
-         #   brand = request.GET['brand']
-          #  products = Product.objects.filter(title__icontains=brand)
-        #if 'title' in request.GET and request.GET['title']:
-         #   title = request.GET['title']
-          #  products = Product.objects.filter(title__icontains=title)
-        #if 'sort_by' in request.GET and request.GET['sort_by']:
-        #    sort_by = request.GET['sort_by']
-        #    if sort_by == 'discount':
-        #        products = products.extra(select={ 'd_field' : '((1 - price/msrp)*100)' }).extra(order_by=['d_field'])
-        #    else:
-        #        products = products.order_by('-'+sort_by)  
-        return render_to_response('search_results.html',
-            {'products': results
-             #'query': q
+      query = request.GET.get('q','')
+      address = query.split(',')
+      
+      result = PrevHomeSales.objects.filter(address__icontains=address[0])
+      print result
+      
+      return render_to_response('search_results.html',
+            {'results': result,
+             'query': query,
              })
     else:
         error = ""
         return render_to_response('search_form.html',
                                   {'error':error})
-
-
-def hello(request):
-	return HttpResponse("Hello world")
-#
-#def current_datetime(request):
-#	now = datetime.datetime.now()
-#	t = get_template('current_datetime.html')
-#	html = t.render(Context({'current_date':now}))
-#	return HttpResponse(html)
-#
-#def hours_ahead(request, offset):
-#	try:
-#		offset = int(offset)
-#	except ValueError:
-#		raise Http404()
-#	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-#	assert False
-#	html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-#	return HttpResponse(html)
