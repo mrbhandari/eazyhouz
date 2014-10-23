378d80d93bd1a202e9f7e4768aac66ea2624e8a1
Rahul Bhandari <mr.bhandari@gmail.com>
2014-10-23 14:36:09 -0700

    update the robots.txt file

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 admin.autodiscover()
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from mysite.views import search, autosuggest
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -13,6 +14,7 @@ urlpatterns = patterns('',
                        (r'^admin/', include(admin.site.urls)),
                        (r'^search/$', search),
                        (r'^autosuggest/$', autosuggest),
+                       (r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 )
 
 urlpatterns += staticfiles_urlpatterns()
