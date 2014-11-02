cfc87c315422b78985a1dacfaacc1975fd083f6a
Pradeep M <pradeep@Users-MacBook-Pro-2.local>
2014-11-01 20:16:56 -0700

    Adding a standalone script for similar houses.

---
+++ mysite/sim_houses_standalone.py
@@ -0,0 +1,64 @@
+import json
+import sys,os
+import datetime
+from decimal import *
+import django
+import heapq
+from django.forms.models import model_to_dict
+import pprint
+
+your_djangoproject_home="/Users/pradeep/eazyhouz/eazyhouz/mysite"
+
+sys.path.append(your_djangoproject_home)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+getcontext().prec = 5
+from search.models import *
+django.setup()
+
+def home_similarity(home,subject_home):
+  return 10 * abs(home.sqft - subject_home.sqft) + 800 * abs(home.sqft - subject_home.sqft)
+
+def gen_appraisal(subject_home):
+  data = {}
+  city = subject_home.city
+  beds = subject_home.beds
+  baths = subject_home.baths
+  sqft = subject_home.sqft
+  min_baths = baths - 0.5
+  max_baths = baths + 0.5
+  min_sqft = sqft * 0.8
+  max_sqft = sqft * 1.2
+  #TODO make sure to not fetch the subject home itself.
+  comp_candidates = PrevHomeSales.objects.filter(beds__exact=beds, baths__lte=max_baths, baths__gte=min_baths, sqft__lte=max_sqft,sqft__gte=min_sqft,city__exact=city)
+  h = []
+  if len(comp_candidates) < 3:
+     return data
+  for c in comp_candidates:
+    sim_score = home_similarity(c,subject_home)
+    heapq.heappush(h,(sim_score,c))
+  k = 3
+  avg_sqft_price = 0
+  
+  for i in range(1,k+1):
+    sim_score,home = heapq.heappop(h)
+    avg_sqft_price += home.sale_price/home.sqft
+    data['home' + str(i)] = model_to_dict(home)
+  avg_sqft_price /= k
+  data['estimated_price'] = avg_sqft_price * subject_home.sqft
+  for i in range(1,k+1):
+    adjustment = {}
+    print str(i) + "th home's sqft" +  str(data['home' + str(i)]['sqft'])
+    adjustment['sqft'] = avg_sqft_price * (subject_home.sqft - data['home'+str(i)]['sqft'])
+    data['adjustment' + str(i)] = adjustment
+  return data
+
+
+prevHome = PrevHomeSales()
+prevHome.beds = 2
+prevHome.city = "san mateo"
+prevHome.baths = 3
+prevHome.sqft=1213
+data = gen_appraisal(prevHome)
+pp = pprint.PrettyPrinter(indent=4)
+print pp.pprint(data)
+
